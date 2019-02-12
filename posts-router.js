45ef1bd5f94c91e94392a13b6550dc2c801061f1
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-12 14:22:10 -0700

    PUT working

--- posts-router.js
+++ posts-router.js
@@ -59,4 +59,30 @@ router.delete('/:id', async(req,res) => {
   }
 });
 
+router.put('/:id', async(req, res) => {
+  try {
+    const putId = req.params.id;
+    const postData = req.body;
+    const numUpdated = await db.update(putId, postData);
+    if (!postData.title || !postData.contents) {
+      res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    } else {
+      if (numUpdated < 1) {
+        res.status(404).json({ message: "The post with the specified ID does not exist." });
+      } else {
+        // PUT was successful, fetch newly updated post
+        console.log("PUT success, fetching updated post.");
+        try {
+          const updatedPost = await db.findById(putId);
+          res.status(200).json({ message: `Successfully updated post id: ${putId}. Updated post: ${JSON.stringify(updatedPost[0])}` });         
+        } catch {
+          res.status(500).json({ error: "Error fetching updated post." });
+        }
+      }
+    }
+  } catch {
+    res.status(500).json({ error: "The post information could not be modified." });
+  }
+});
+
 module.exports = router;
