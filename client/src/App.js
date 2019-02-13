0d7f6e0464d4601bd388f11b11b795ea7fa7d79d
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-12 17:08:12 -0700

    add post working

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,56 @@ import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 import axios from 'axios';
 import './App.css';
+
+const emptyCommentForm = {
+  title: "",
+  contents: "",
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      comments: []
+      posts: [],
+      ...emptyCommentForm
     }
+  }
+
+  handleChanges = (e) => {
+    this.setState ({
+      [e.target.name]: e.target.value
+    });
   }
 
   getComments() {
     axios.get('http://localhost:4001/api/posts')
-    .then(res => {
-      this.setState({
-        comments: res.data
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          posts: res.data
+        })
       })
-    })
-    .catch(err => console.log(err));
+      .catch(err => console.log(err));
+  }
+
+  addPost() {
+    const postFormData = {
+      title: this.state.title,
+      contents: this.state.contents
+    }
+    axios.post('http://localhost:4001/api/posts', postFormData)
+      .then(res => {
+        // console.log(res.data.message);
+        this.setState({ posts: res.data.message });
+      })
+      .catch(err => {
+        console.log(`error addComment: ${err}`);
+      })
   }
 
   componentDidMount() {
@@ -29,25 +59,68 @@ class App extends Component {
   }
 
   render() {
-    const comments = this.state.comments.map( comment => {
+    const posts = this.state.posts.map( post => {
       return (
-        <Card key={comment.id} 
+        <Card key={post.id} 
           raised 
           style={{ 
             margin: "15px",
             padding: "15px", 
-            maxWidth: "250px" 
+            maxWidth: "250px",
+            textAlign: "left"
           }}
         >
-          <Typography variant="h5">Comment ID: {comment.id}</Typography>
-          <Typography variant="body1">{comment.title}</Typography>
-          <Typography variant="body2">{comment.contents}</Typography>
+          <Typography variant="h5">Post ID: {post.id}</Typography>
+          <Typography variant="body1">{post.title}</Typography>
+          <Typography variant="body2">{post.contents}</Typography>
         </Card>
       );
     });
     return (
       <div className="App">
         <Typography variant="h3">Welcome to Posts</Typography>
+        <Typography variant="h4">Add Post</Typography>
+        <Grid 
+          container
+          direction="row"
+          justify="space-evenly"
+          style={{
+            padding: "25px"
+          }}
+        >
+          <Card
+            raised 
+            style={{ 
+              padding: "15px", 
+              maxWidth: "250px",
+              textAlign: "left"
+            }}
+          >
+            <form>
+              <TextField
+                variant="outlined"
+                label="Title"
+                name="title"
+                value={this.state.title}
+                onChange={this.handleChanges}
+                autoComplete="off"
+                style={{ width: '100%', paddingBottom: "15px" }}
+              />
+              <TextField
+                variant="outlined"
+                label="Contents"
+                name="contents"
+                value={this.state.contents}
+                onChange={this.handleChanges}
+                autoComplete="off"
+                style={{ width: '100%', paddingBottom: "15px" }}
+              />
+              <Button
+                onClick={() => this.addPost()}
+              >Add Post</Button>
+            </form>
+          </Card>
+        </Grid>
         <Typography variant="h4">Current Posts:</Typography>
         <Grid 
           container
@@ -57,7 +130,7 @@ class App extends Component {
             padding: "50px"
           }}
         >
-          {comments}
+          {posts}
         </Grid>
       </div>
     );
