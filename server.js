c7ffcc4f95aa8af514cbad113445363f973a9870
kjohna <43828763+kjohna@users.noreply.github.com>
2019-02-14 15:03:06 -0700

    changes to deploy client on heroku

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const postsRouter = require('./posts-router.js');
 
 const server = express();
+
+// guide for heroku deploy: https://medium.freecodecamp.org/how-to-deploy-a-react-app-with-an-express-server-on-heroku-32244fe5a250
+
+server.use(express.static(path.join(__dirname, 'client/build')));
+
+// production mode:
+if(process.env.NODE_ENV === 'production') {
+  server.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  server.get('/', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+
+// build mode:
+server.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
 
 server.use(express.json());
 server.use(cors());
 
 server.use('/api/posts', postsRouter);  // posts route handlers
 
-server.get('/', (req, res) => {
-  res.send(`<h1>Hello from Server!</h1>`);
-});
+// // for initial testing: 
+// server.get('/', (req, res) => {
+//   res.send(`<h1>Hello from Server!</h1>`);
+// });
 
 module.exports = server;
